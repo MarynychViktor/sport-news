579f3fde8dbbce7792cf594393b984dc1a26f019
Marynych Viktor <vmaryn@softserveinc.com>
2021-04-22 11:36:44 +0300

    [Tests]   - dropped autogenerated tests

--- test/controllers/admin/home_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Admin::HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    assert_equal 1, 1
-  end
-end
--- test/controllers/home_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    assert_equal 1, 1
-  end
-end
