1e7c02722127028de652f9c61b47bf105157ef71
Marynych Viktor <vmaryn@softserveinc.com>
2021-05-13 17:12:55 +0300

    [Refactoring]   - refactoring

--- app/controllers/customer/articles_controller.rb
+++ app/controllers/customer/articles_controller.rb
@@ -4,13 +4,21 @@ module Customer
       @most_commented = Article.most_commented(max: 3)
       @most_popular = Article.most_popular(max: 3)
 
-      @hero_articles = Article.published.take(1)
-      @other_articles = Article.published.offset(1).take(4)
+      @articles = Article.from_query(search_params)
+      @hero_articles = @articles.take(1)
+      @other_articles = @articles.offset(1).take(4)
       @article = Article.first
     end
 
     def show
       @article = Article.friendly.find(params[:id])
+    end
+
+    private
+
+    def search_params
+      query = params.permit(:category_id, :subcategory_id, :team_id).select { |k, v| v && !v.empty? }
+      { where: query, scopes: %i[published] }
     end
   end
 end
