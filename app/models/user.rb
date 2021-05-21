c9c2b154bb5b6748d546c175ee74ac5c7d82910a
Marynych Viktor <vmaryn@softserveinc.com>
2021-05-21 12:01:12 +0300

    [Oauth] Google Provider   - added google oauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,11 @@
 #  first_name             :string           not null
 #  last_name              :string           not null
 #  photo                  :string           not null
+#  provider               :string
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  uid                    :string
 #  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
@@ -25,18 +27,15 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  GOOGLE_PROVIDER = :google_oauth2
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
-  #  TODO: add uploader to S3
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  devise :omniauthable, omniauth_providers: [GOOGLE_PROVIDER]
   mount_uploader :photo, PhotoUploader
   has_many :comments
 
   validates :first_name, :last_name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :photo, presence: true
+  validates :photo, presence: true, if: -> { remote_photo_url.present? }
   validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\-_+!]{8,}\Z/}
 
   def full_name
