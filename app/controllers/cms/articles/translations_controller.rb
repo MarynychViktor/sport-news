fd41310a6bc2b88f1b36f867a6cc85304d828ba7
Marynych Viktor <vmaryn@softserveinc.com>
2021-05-28 15:22:28 +0300

    [Languages]   - refactoring

--- app/controllers/cms/articles/translations_controller.rb
+++ app/controllers/cms/articles/translations_controller.rb
@@ -1,40 +1,33 @@
-module CMS::Articles
-  class TranslationsController < CMS::ApplicationController
-    before_action :find_article_and_category
-    around_action :apply_locale_context
+# frozen_string_literal: true
+module CMS
+  module Articles
+    class TranslationsController < CMS::ApplicationController
+      before_action :find_article_and_category
+      around_action :apply_locale_context
 
-    def edit
-      # @locale = params[:id].to_sym
-      # @article = Article.find(params[:article_id])
-      # @category = @article.category
-    end
-
-    def update
-      if @article.update(article_params)
-        return redirect_to edit_cms_category_article_path(@category, @article)
-      else
-        render :edit
-      # @locale = params[:id].to_sym
-      # @article = Article.find(params[:article_id])
-      # @category = @article.category
-      #
+      def update
+        if @article.update(article_params)
+          redirect_to edit_cms_category_article_path(@category, @article)
+        else
+          render :edit
+        end
       end
-    end
 
-    private
+      private
 
-    def find_article_and_category
-      @locale = params[:id]
-      @article = Article.friendly.find(params[:article_id])
-      @category = @article.category
-    end
+      def find_article_and_category
+        @locale = params[:id]
+        @article = Article.friendly.find(params[:article_id])
+        @category = @article.category
+      end
 
-    def apply_locale_context(&action)
-      Mobility.with_locale(params[:id].to_sym, &action)
-    end
+      def apply_locale_context(&action)
+        Mobility.with_locale(params[:id].to_sym, &action)
+      end
 
-    def article_params
-      params.require(:article).permit(:headline, :alt, :caption, :content)
+      def article_params
+        params.require(:article).permit(:headline, :alt, :caption, :content)
+      end
     end
   end
 end
